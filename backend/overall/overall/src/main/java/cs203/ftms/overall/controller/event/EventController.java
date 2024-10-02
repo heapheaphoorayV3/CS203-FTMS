c0581e477c882090841a6000e80fcfd76c93c3ff
WastefulPines <ngjackin2016@gmail.com>
2024-10-02 16:34:57 +0800

    edit http request for register event from get to put

--- backend/overall/overall/src/main/java/cs203/ftms/overall/controller/event/EventController.java
+++ backend/overall/overall/src/main/java/cs203/ftms/overall/controller/event/EventController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +55,7 @@ public class EventController {
         return new ResponseEntity<>("event creation unsuccessful", HttpStatus.BAD_REQUEST);
     }
 
-    @GetMapping("/register/{eid}")
+    @PutMapping("/register/{eid}")
     @PreAuthorize("hasRole('FENCER')")
     public ResponseEntity<String> registerEvent(@PathVariable int eid) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
