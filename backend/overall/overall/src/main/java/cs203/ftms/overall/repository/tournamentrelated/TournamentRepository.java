b7ce8babfb28cafa5548b1293c56d29a1b89f6ad
WastefulPines <ngjackin2016@gmail.com>
2024-10-16 12:37:28 +0800

    added get tournaments by an organiser

--- backend/overall/overall/src/main/java/cs203/ftms/overall/repository/tournamentrelated/TournamentRepository.java
+++ backend/overall/overall/src/main/java/cs203/ftms/overall/repository/tournamentrelated/TournamentRepository.java
@@ -1,5 +1,6 @@
 package cs203.ftms.overall.repository.tournamentrelated;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,5 @@ import cs203.ftms.overall.model.tournamentrelated.Tournament;
 
 public interface TournamentRepository extends JpaRepository<Tournament, Integer> {
     Optional<Tournament> findByName(String name);
+    Optional<List<Tournament>> findByOrganiserId(int id);
 }
