c42f75269bb0a4e5d27a8a29e087120754ad55ce
Yap Zu Kai <yapzukai@gmail.com>
2024-10-26 22:44:30 +0800

    unit testing for authentication service

--- backend/overall/overall/src/test/java/cs203/ftms/overall/AuthenticationServiceTest.java
+++ backend/overall/overall/src/test/java/cs203/ftms/overall/AuthenticationServiceTest.java
@@ -1,21 +1,20 @@
 package cs203.ftms.overall;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.time.LocalDate;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,6 +23,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import cs203.ftms.overall.dto.RegisterAdminDTO;
 import cs203.ftms.overall.dto.RegisterFencerDTO;
 import cs203.ftms.overall.dto.RegisterOrganiserDTO;
+import cs203.ftms.overall.dto.clean.CleanFencerDTO;
+import cs203.ftms.overall.dto.clean.CleanOrganiserDTO;
+import cs203.ftms.overall.exception.UserAlreadyExistException;
 import cs203.ftms.overall.model.userrelated.Admin;
 import cs203.ftms.overall.model.userrelated.Fencer;
 import cs203.ftms.overall.model.userrelated.Organiser;
@@ -31,6 +33,7 @@ import cs203.ftms.overall.model.userrelated.User;
 import cs203.ftms.overall.repository.userrelated.UserRepository;
 import cs203.ftms.overall.service.admin.MailService;
 import cs203.ftms.overall.service.authentication.AuthenticationService;
+import jakarta.persistence.EntityNotFoundException;
 
 class AuthenticationServiceTest {
 
@@ -55,6 +58,66 @@ class AuthenticationServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
+        @Test
+    void getCleanFencer_ValidFencer_ReturnsCleanFencerDTO() {
+        // Arrange
+        Fencer fencer = new Fencer();
+        fencer.setId(1);
+        fencer.setName("John Doe");
+        fencer.setEmail("john.doe@example.com");
+        fencer.setContactNo("+6591234567");
+        fencer.setCountry("Singapore");
+        fencer.setDateOfBirth(LocalDate.of(2000, 1, 1));
+        fencer.setDominantArm('R');
+        fencer.setWeapon('F');
+        fencer.setClub("Singapore Fencing Club");
+        fencer.setPoints(100);
+        fencer.setDebutYear(2018);
+        fencer.setGender('M');
+
+        // Act
+        CleanFencerDTO cleanFencerDTO = authenticationService.getCleanFencer(fencer);
+
+        // Assert
+        assertNotNull(cleanFencerDTO);
+        assertEquals(fencer.getId(), cleanFencerDTO.getId());
+        assertEquals(fencer.getName(), cleanFencerDTO.getName());
+        assertEquals(fencer.getEmail(), cleanFencerDTO.getEmail());
+        assertEquals(fencer.getContactNo(), cleanFencerDTO.getContactNo());
+        assertEquals(fencer.getCountry(), cleanFencerDTO.getCountry());
+        assertEquals(fencer.getDateOfBirth(), cleanFencerDTO.getDateOfBirth());
+        assertEquals(fencer.getDominantArm(), cleanFencerDTO.getDominantArm());
+        assertEquals(fencer.getWeapon(), cleanFencerDTO.getWeapon());
+        assertEquals(fencer.getClub(), cleanFencerDTO.getClub());
+        assertEquals(fencer.getPoints(), cleanFencerDTO.getPoints());
+        assertEquals(fencer.getDebutYear(), cleanFencerDTO.getDebutYear());
+        assertEquals(fencer.getGender(), cleanFencerDTO.getGender());
+    }
+
+    @Test
+    void getCleanOrganiser_ValidOrganiser_ReturnsCleanOrganiserDTO() {
+        // Arrange
+        Organiser organiser = new Organiser();
+        organiser.setId(1);
+        organiser.setVerified(true);
+        organiser.setName("Organizer One");
+        organiser.setEmail("organizer.one@example.com");
+        organiser.setContactNo("+6599876543");
+        organiser.setCountry("Singapore");
+
+        // Act
+        CleanOrganiserDTO cleanOrganiserDTO = authenticationService.getCleanOrganiser(organiser);
+
+        // Assert
+        assertNotNull(cleanOrganiserDTO);
+        assertEquals(organiser.getId(), cleanOrganiserDTO.getId());
+        assertEquals(organiser.isVerified(), cleanOrganiserDTO.isVerified());
+        assertEquals(organiser.getName(), cleanOrganiserDTO.getName());
+        assertEquals(organiser.getEmail(), cleanOrganiserDTO.getEmail());
+        assertEquals(organiser.getContactNo(), cleanOrganiserDTO.getContactNo());
+        assertEquals(organiser.getCountry(), cleanOrganiserDTO.getCountry());
+    }
+
     @Test
     void createFencer_ValidFields_Success() {
         // Arrange
@@ -73,6 +136,18 @@ class AuthenticationServiceTest {
         
         verify(passwordEncoder).encode(registerFencerDTO.getPassword());
         verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void createFencer_AlreadyExist_Failure() {
+        // Arrange
+        RegisterFencerDTO registerFencerDTO = new RegisterFencerDTO("John", "Doe", "john.doe@example.com", "Abcd1234!",
+                "+6591238765", "Singapore", LocalDate.of(2000, 1, 1));
+
+        when(userRepository.findByEmail(registerFencerDTO.getEmail())).thenReturn(Optional.of(new Fencer()));
+
+        // Act & Assert
+        assertThrows(UserAlreadyExistException.class, () -> authenticationService.createFencer(registerFencerDTO));
     }
 
     @Test
@@ -96,6 +171,17 @@ class AuthenticationServiceTest {
     }
 
     @Test
+    void createOrganiser_AlreadyExist_Failure() {
+        // Arrange
+        RegisterOrganiserDTO registerOrganiserDTO = new RegisterOrganiserDTO("Organizer One", "organizer.one@example.com", "Abcd1234!", "+6591238765", "Singapore");
+
+        when(userRepository.findByEmail(registerOrganiserDTO.getEmail())).thenReturn(Optional.of(new Organiser()));
+
+        // Act & Assert
+        assertThrows(UserAlreadyExistException.class, () -> authenticationService.createOrganiser(registerOrganiserDTO));
+    }
+
+    @Test
     void createAdmin_ValidFields_ReturnTrue() {
         // Arrange
         RegisterAdminDTO registerAdminDTO = new RegisterAdminDTO("Admin One", "admin.one@example.com", "Abcd1234!", "+6591238765", "Singapore");
@@ -112,6 +198,17 @@ class AuthenticationServiceTest {
 
         verify(passwordEncoder).encode(registerAdminDTO.getPassword());
         verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void createAdmin_AlreadyExist_Failure() {
+        // Arrange
+        RegisterAdminDTO registerAdminDTO = new RegisterAdminDTO("Admin One", "admin.one@example.com", "Abcd1234!", "+6591238765", "Singapore");
+
+        when(userRepository.findByEmail(registerAdminDTO.getEmail())).thenReturn(Optional.of(new Admin()));
+
+        // Act & Assert
+        assertThrows(UserAlreadyExistException.class, () -> authenticationService.createAdmin(registerAdminDTO));
     }
 
     @Test
@@ -138,7 +235,29 @@ class AuthenticationServiceTest {
     }
 
     @Test
-    void refreshToken_ValidToken_ReturnTrue() {
-        
+    void getUser_ValidId_ReturnUser() {
+        // Arrange
+        int userId = 1;
+        User user = new Fencer();
+        user.setId(userId);
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // Act
+        User foundUser = authenticationService.getUser(userId);
+
+        // Assert
+        assertNotNull(foundUser);
+        assertEquals(userId, foundUser.getId());
+        verify(userRepository).findById(userId);
+    }
+
+    @Test
+    void getUser_InvalidId_ThrowsException() {
+        // Arrange
+        int userId = 1;
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(EntityNotFoundException.class, () -> authenticationService.getUser(userId));
     }
 }
