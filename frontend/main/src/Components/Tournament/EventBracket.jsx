fe607c51c614cb3b54d5232c0be2c9157bf4d0af
anlhq <andreanlhq@gmail.com>
2024-10-28 22:11:08 +0800

    Edit bracket colours

--- frontend/main/src/Components/Tournament/EventBracket.jsx
+++ frontend/main/src/Components/Tournament/EventBracket.jsx
@@ -1,46 +1,48 @@
-import { SingleEliminationBracket, Match, SVGViewer, createTheme } from '@g-loot/react-tournament-brackets';
+import {
+  SingleEliminationBracket,
+  Match,
+  SVGViewer,
+  createTheme,
+} from "@g-loot/react-tournament-brackets";
 // import { GlootTheme, WhiteTheme } from '../Others/EventBracketTheme';
-import CustomMatch from '../Others/CustomMatch';
-import React from 'react';
-
+import CustomMatch from "../Others/CustomMatch";
+import React from "react";
 
 export default function EventBracket({ matches, height, width }) {
-    console.log("Height: ", height + " Width: ", width);
-    return (
-        <SingleEliminationBracket
-            theme={WhiteTheme}
-            matches={matches}
-            matchComponent={CustomMatch}
-            svgWrapper={({ children, ...props }) => (
-                <SVGViewer
-                    width={width}
-                    height={height}
-                    background={WhiteTheme.svgBackground}
-                    SVGBackground={WhiteTheme.svgBackground}
-                    {...props}
-                >
-                    {children}
-                </SVGViewer>
-            )}
-        />
-    );
+  console.log("Height: ", height + " Width: ", width);
+  return (
+    <SingleEliminationBracket
+      theme={WhiteTheme}
+      matches={matches}
+      matchComponent={CustomMatch}
+      svgWrapper={({ children, ...props }) => (
+        <SVGViewer
+          width={width}
+          height={height}
+          background={WhiteTheme.svgBackground}
+          SVGBackground={WhiteTheme.svgBackground}
+          {...props}
+        >
+          {children}
+        </SVGViewer>
+      )}
+    />
+  );
 }
 
-
 export const WhiteTheme = createTheme({
-    textColor: { main: '#000000', highlighted: '#07090D', dark: '#3E414D' },
-    matchBackground: { wonColor: '#daebf9', lostColor: '#96c6da' },
-    score: {
-        background: { wonColor: '#87b2c4', lostColor: '#87b2c4' },
-        text: { highlightedWonColor: '#7BF59D', highlightedLostColor: '#FB7E94' },
-    },
-    border: {
-        color: '#CED1F2',
-        highlightedColor: '#da96c6',
-    },
-    roundHeader: { backgroundColor: '#da96c6', fontColor: '#000' },
-    connectorColor: '#CED1F2',
-    connectorColorHighlight: '#da96c6',
-    svgBackground: '#FAFAFA',
+  textColor: { main: "#000000", highlighted: "#07090D", dark: "#3E414D" },
+  matchBackground: { wonColor: "#cfdee7", lostColor: "#cfdee7" },
+  score: {
+    background: { wonColor: "#f37748", lostColor: "#00a7e1" },
+    text: { highlightedWonColor: "#FFFFFF", highlightedLostColor: "#FFFFFF" },
+  },
+  border: {
+    color: "#edf2fb",
+    highlightedColor: "#f37748",
+  },
+  roundHeader: { backgroundColor: "#4472ca", fontColor: "#FFFFFF" },
+  connectorColor: '#CED1F2',
+  connectorColorHighlight: '#da96c6',
+  svgBackground: "#FFFFFF",
 });
-
