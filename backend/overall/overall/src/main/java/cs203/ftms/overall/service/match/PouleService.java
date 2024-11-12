47e33a1c3e28400c5e22c579be76cdcbf351e56c
Yap Zu Kai <yapzukai@gmail.com>
2024-11-12 18:01:16 +0800

    Update PouleService.java

--- backend/overall/overall/src/main/java/cs203/ftms/overall/service/match/PouleService.java
+++ backend/overall/overall/src/main/java/cs203/ftms/overall/service/match/PouleService.java
@@ -495,10 +495,10 @@ public class PouleService {
         if (pouleMatch.getScore1() > pouleMatch.getScore2()) {
             fencer1.setPouleWins(fencer1.getPouleWins() + 1);
             pouleMatch.setWinner(fencer1.getId());
-        } else {
+        } else if(pouleMatch.getScore1() < pouleMatch.getScore2()) {
             fencer2.setPouleWins(fencer2.getPouleWins() + 1);
             pouleMatch.setWinner(fencer2.getId());
-        }
+        } 
 
         tournamentFencerRepository.save(fencer1);
         tournamentFencerRepository.save(fencer2);
